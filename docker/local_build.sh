070e18419d20ce4bf71e193f4f50e23339885997
Max Howard <max@nanome.ai>
2020-01-16 16:35:14 -0800

    removed docker volume code

--- docker/local_build.sh
+++ docker/local_build.sh
@@ -1,8 +1,1 @@
-if [[ $(docker volume ls -f name=url-loader-volume -q) ]]; then
-    echo "Skipping volume creation"
-else
-    echo "Creating new docker volume"
-    docker volume create url-loader-volume
-fi
-
 docker build -f urlloader.Dockerfile -t urlloader:latest ..
